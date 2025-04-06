e373d29e21a1174578a8ff621b3bbda2d76f2d58
Axel Lopez <axel@cohort42.com>
2025-04-06 11:08:06 +0200

    Added main carousel

--- apps/front/src/routes/(main)/+page.svelte
+++ apps/front/src/routes/(main)/+page.svelte
@@ -1,12 +1,15 @@
 <script>
   import Sidebar from './Sidebar.svelte';
   import Header from './Header.svelte';
+  import Carousel from './Carousel.svelte';
 </script>
 
 <div class="flex h-screen flex-col bg-background">
   <Header />
   <div class="flex h-full w-full">
     <Sidebar />
-    <div class="flex h-full w-full flex-col gap-4"></div>
+    <div class="flex h-full w-full flex-col gap-4">
+      <Carousel items={[]} />
+    </div>
   </div>
 </div>
---
+++ apps/front/src/routes/(main)/Carousel.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { Carousel } from 'hevel-ui';
+  import Autoplay from 'embla-carousel-autoplay';
+  import type { Component } from 'svelte';
+
+  let { items }: { items: Component[] } = $props();
+
+  let api = $state<any>();
+
+  const count = $derived(api ? api.scrollSnapList().length : 0);
+  let current = $state(0);
+
+  $effect(() => {
+    if (api) {
+      current = api.selectedScrollSnap() + 1;
+      api.on('select', () => {
+        current = api!.selectedScrollSnap() + 1;
+      });
+    }
+  });
+</script>
+
+{#snippet Item(Item: Component)}
+  <Carousel.Item class="h-full w-full p-0">
+    <div class="flex h-full items-center justify-center p-6">
+      <Item />
+    </div>
+  </Carousel.Item>
+{/snippet}
+
+<div class="flex h-full w-full flex-col gap-4 p-4">
+  <Carousel.Root
+    plugins={[
+      Autoplay({
+        delay: 5000
+      })
+    ]}
+    setApi={(emblaApi) => (api = emblaApi)}
+    class="h-full w-full [&>*]:h-full"
+  >
+    <Carousel.Content class="mx-0 h-full w-full">
+      {#each items as item}
+        {@render Item(item)}
+      {/each}
+    </Carousel.Content>
+  </Carousel.Root>
+  <div class="flex items-center justify-center gap-2 py-4">
+    {#each Array(count) as _, i (i)}
+      <div class="h-2 w-2 rounded-full {current === i + 1 ? 'bg-primary' : 'bg-muted-foreground'}"></div>
+    {/each}
+  </div>
+</div>
